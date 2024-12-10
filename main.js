b19039dc43417f0ffd6036c03108e52bb9b56bf0
sarav929 <ec2325508@edinburghcollege.ac.uk>
2024-12-10 14:52:42 +0000

    create equals function

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
-<div>
-    <div></div>
-    <btn></btn>
+function add(num1, num2) {
+    return num1 + num2
+}
 
+function multiply(num1, num2) {
+    return num1 * num2
+}
 
-</div>
+function subtract(num1, num2) {
+    return num1 - num2
+}
+
+function divide(num1, num2) {
+    if (num2 == 0) {
+        return "Infinity"
+    }
+    return num1 / num2
+}
+
+function equals(operator, num1, num2) {
+    if (operator == "+") {
+        return add(num1, num2)
+    } else if (operator == "-") {
+        return subtract(num1, num2) 
+    } else if (operator == "*") {
+        return multiply(num1, num2) 
+    } else if (operator == "/") {
+        return divide(num1, num2) 
+    } else {
+        return 'error'
+    }
+}
