978f6d7e6d9f8df4d8794491a0f3338307cc0856
sarav929 <ec2325508@edinburghcollege.ac.uk>
2024-12-10 14:44:12 +0000

    create division operation

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-<div>
-    <div></div>
-    <btn></btn>
+function add(num1, num2) {
+    return num1 + num2
+}
 
+function multiply(num1, num2) {
+    return num1 * num2
+}
 
-</div>
+function subtract(num1, num2) {
+    return num1 - num2
+}
+
+function divide(num1, num2) {
+    if (num2 == 0) {
+        return "Infinity"
+    }
+    return num1 / num2
+}
+
