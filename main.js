83b241c92627bfe94d81962a62928913ecf699b4
George Ramsey <georgeramsey@live.com>
2024-12-11 10:54:09 +0000

    fix semi colons and styling

--- main.js
+++ main.js
@@ -1,82 +1,84 @@
 function add(num1, num2) {
-    return num1 + num2
+  return num1 + num2;
 }
 
 function multiply(num1, num2) {
-    return num1 * num2
+  return num1 * num2;
 }
 
 function subtract(num1, num2) {
-    return num1 - num2
+  return num1 - num2;
 }
 
 function divide(num1, num2) {
-    if (num2 == 0) {
-        return "Infinity"
-    }
-    return num1 / num2
+  if (num2 == 0) {
+    return "Infinity";
+  }
+  return num1 / num2;
 }
 
 function equals(operator, num1, num2) {
-    
-    if (operator == "+") {
-        return add(num1, num2)
-    } else if (operator == "-") {
-        return subtract(num1, num2) 
-    } else if (operator == "*") {
-        return multiply(num1, num2) 
-    } else if (operator == "/") {
-        return divide(num1, num2) 
-    } else {
-        return 'error'
-    }
+  if (operator == "+") {
+    return add(num1, num2);
+  } else if (operator == "-") {
+    return subtract(num1, num2);
+  } else if (operator == "x") {
+    return multiply(num1, num2);
+  } else if (operator == "÷") {
+    return divide(num1, num2);
+  } else {
+    return "error";
+  }
 }
 
-let display = ''
-let num1 = null
-let num2 = null
-let operator = ''
-let lastBtnPressed = ''
+let display = "";
+let num1 = null;
+let num2 = null;
+let operator = "";
+let operatorSelected = false;
 
-const operatorBtns = document.querySelectorAll('.operators')
-const screen = document.querySelector('.screen')
-const equal = document.querySelector('.equal')
+const operatorBtns = document.querySelectorAll(".operators");
+const screen = document.querySelector(".screen");
+const equal = document.querySelector(".equal");
+const clear = document.querySelector(".clear");
+
+if (operatorSelected == true) {
+  operatorBtns.forEach((btn) => {
+    btn.disabled = true;
+  });
+}
 
 operatorBtns.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        
-        if (operator == '') {
+  btn.addEventListener("click", () => {
+    if (operatorSelected == false) {
+      operatorSelected = true;
+      operator = btn.textContent;
+      display += operator;
+      screen.textContent = display;
+    }
+  });
+});
 
-            operator = btn.textContent
-
-            //if (num1 == null) {
-                //num1 = parseInt(display)
-            //} else {
-               //num2 = parseInt(display)
-            //}
-            //display = ''
-            
-            screen.textContent = display
-        } else {
-            // execute current operation with equals() //
-        } 
-        operator = ''       
-    })
-})
-
-const numberBtns = document.querySelectorAll('.numbers')
+const numberBtns = document.querySelectorAll(".numbers");
 
 numberBtns.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        
-        display += btn.id
-        screen.textContent += btn.id
-        
-    })
-})
+  btn.addEventListener("click", () => {
+    display += btn.id;
+    screen.textContent += btn.id;
+  });
+});
 
-equal.addEventListener('click', () => {
-    console.log(num1)
-    console.log(num2)
-    screen.textContent = equals(operator, num1, num2)
-})
+equal.addEventListener("click", () => {
+  const result = display.split(/[+\-x÷\s]/);
+  num1 = Number(result[0]);
+  num2 = Number(result[1]);
+  operator = display.match(/[+\-x÷\s]/)[0];
+
+  screen.textContent = equals(operator, num1, num2);
+  operatorSelected = false;
+
+  num1 = equals(operator, num1, num2);
+  operator = "";
+  num2 = null;
+  display = num1;
+});
